b3cf6045c38059eeafe6fac4ffbbfeed4a3b7d11
AJ <ajmorr19@harrisonburg.k12.va.us>
2019-09-19 09:44:06 -0400

    Douglas D. Dimmadome

--- game.js
+++ game.js
@@ -1,16 +1,19 @@
-var score = 1; // Create a variable to store the score in
+var score = 0; // Create a variable to store the score in
 
 postToPage(score);
-changeTitle('Dimmsdale Dimmadome');
+changeTitle('Clicker Game');
 
+function scoreIncrease() {
+  score++;                       // Increase the score
+  createButton('Press Me ~UwU~', btnPress);
 
- 
-setInterval(btnPress, 100); //the timer
-
+  if(score > 10) {
+    createButton('Hewwo', btnPress)               // If the score reaches 11, show the button!
+  }
+}
 
 function btnPress() {
-  score = score + 10000000000000000000000000000000000000001000000000000000000000000000000000000000100000000000000000000000000000010000000000000000000000000000000000000001000000000000000000000000000000000000000100000000000000000000000000000000000000010000000000000000000000000000000000000001000000000000000000000000000000000000000000000000;
-  createButton('Doug Dimmadome, Owner of the Dimmsdale Dimmadome', btnPress2);
+  score = score + 1;
   postToPage(score);
 }
 function btnPress2(){
