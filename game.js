15c309dbc3b2847a9ae6aef2042a7a3982c005a4
AJ <ajmorr19@harrisonburg.k12.va.us>
2019-09-23 09:54:07 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,32 +1,11 @@
-var score = 0; // Create a variable to store the score in
+var welcome = new Title('Hewwo');
+var woodButton = new Button ('Collect Wood', increaseWood);
+var wood = 0;
+var woodCount = new Text(wood)
 
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
-hidden.hide();
-var scr = new Text(score);                    // Create a text element
 
-function scoreIncrease() {
-  score++;                       // Increase the score
-  createButton('Press Me ~UwU~', btnPress);
 
-  if(score > 10) {
-    createButton('Hewwo', btnPress)               // If the score reaches 11, show the button!
-  }
+function increaseWood(){
+  wood++;
+  woodCount.edit(wood);
 }
-
-function btnPress() {
-  score++;         // Increase the score
-  scr.edit(score); // Update the page with the new score
-  if(score > 10) {
-    hidden.show();
-  }
-  if(score > 15) {
-    hidden.hide(500);
-  }
-}
-function btnPress2(){
-  score--;
-  postToPage(score)
-}
-
