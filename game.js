82cea5a463f056347ee7f19566e56e513d06bbf2
AJ <ajmorr19@harrisonburg.k12.va.us>
2019-09-24 09:05:55 -0400

    Idle Game
    Aj's Idle Game

--- game.js
+++ game.js
@@ -1,11 +1,32 @@
 var welcome = new Title('Hewwo');
-var woodButton = new Button ('Collect Wood', increaseWood);
+var woodButton = new Button ('Chop Wood', increaseWood);
+var stoneButton = new Button ('Mine Stone', increaseStone);
+var coalButton = new Button ('Mine Coal', increaseCoal);
+var ironButton = new Button ('Mine Iron', increaseIron);
 var wood = 0;
-var woodCount = new Text(wood)
+var woodCount = new Text(wood + ' Wood')
+var stone = 0;
+var stoneCount = new Text(stone + ' Stone')
+var coal = 0;
+var coalCount = new Text(coal + ' Coal')
+var iron = 0;
+var ironCount = new Text(iron + ' Iron')
 
 
 
 function increaseWood(){
   wood++;
-  woodCount.edit(wood);
+  woodCount.edit(wood + ' Wood');
+}
+function increaseStone(){
+  stone++;
+  stoneCount.edit(stone + ' Stone');
+}
+function increaseCoal(){
+  coal++;
+  coalCount.edit(coal + ' Coal');
+}
+function increaseIron(){
+  iron++;
+  ironCount.edit(iron + ' Iron');
 }
