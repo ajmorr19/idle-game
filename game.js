9f9ff5b9bc9becbf130943cb788d617a0c1cc1f8
AJ <ajmorr19@harrisonburg.k12.va.us>
2019-09-27 09:43:55 -0400

    Idle Game AJ
    AJ IDLE GAME

--- game.js
+++ game.js
@@ -7,18 +7,18 @@ let NewMineButton = new button ('Build a mine', buildMine)
 let mine = 0;
 let mineCount = new Text(mine + 'Mine(s)')
 NewMineButton.hide()
-let stoneButton = new Button ('Mine Stone', increaseStone);
-let stone = 0;
-let stoneCount = new Text(stone + ' Stone')
-hide.button()
-let coalButton = new Button ('Mine Coal', increaseCoal);
-let coal = 0;
-let coalCount = new Text(coal + ' Coal')
-hide.button()
-let ironButton = new Button ('Mine Iron', increaseIron);
-let iron = 0;
-let ironCount = new Text(iron + ' Iron')
-hide.button()
+//let stoneButton = new Button ('Mine Stone', increaseStone);
+//let stone = 0;
+//let stoneCount = new Text(stone + ' Stone')
+//hide.button()
+//let coalButton = new Button ('Mine Coal', increaseCoal);
+//let coal = 0;
+//let coalCount = new Text(coal + ' Coal')
+//hide.button()
+//let ironButton = new Button ('Mine Iron', increaseIron);
+//let iron = 0;
+//let ironCount = new Text(iron + ' Iron')
+//hide.button()
 
 function NewMine(){
 if(wood > 25){
@@ -36,15 +36,15 @@ function increaseWood(){
   woodCount.edit(wood + ' Wood');
   
 }
-function increaseStone(){
-  stone++;
-  stoneCount.edit(stone + ' Stone');
-}
-function increaseCoal(){
-  coal++;
-  coalCount.edit(coal + ' Coal');
-}
-function increaseIron(){
-  iron++;
-  ironCount.edit(iron + ' Iron');
-}
+//function increaseStone(){
+  //stone++;
+  //stoneCount.edit(stone + ' Stone');
+//}
+//function increaseCoal(){
+  //coal++;
+  //coalCount.edit(coal + ' Coal');
+//}
+//function increaseIron(){
+  //iron++;
+  //ironCount.edit(iron + ' Iron');
+//}
